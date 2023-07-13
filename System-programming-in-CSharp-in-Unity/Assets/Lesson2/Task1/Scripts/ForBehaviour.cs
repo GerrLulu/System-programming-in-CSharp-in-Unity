77f9fe2aa1ea7e82e6ad7d6fc05e8119d6f6eb48
Artyom Dyomin <88819724+GerrLulu@users.noreply.github.com>
2023-07-13 15:34:18 +0300

    Lesson2Task1
    Completed

---
+++ System-programming-in-CSharp-in-Unity/Assets/Lesson2/Task1/Scripts/ForBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace LessonTwo.TaskOne
+{
+    public class ForBehaviour : MonoBehaviour
+    {
+        [SerializeField] private int _sizeArray;
+        private NativeArray<int> _array;
+        private JobHandle _jobHandle;
+
+        void Start()
+        {
+            _array = new NativeArray<int>(_sizeArray, Allocator.TempJob);
+            for(int i = 0;  i < _sizeArray; i++)
+            {
+                _array[i] = Random.Range(0, 100);
+                Debug.Log($"Befor [{i}] = {_array[i]}");
+            }
+
+
+            TaskOneLessonTwo taskOneLessonTwo = new TaskOneLessonTwo();
+            taskOneLessonTwo.Array = _array;
+
+            _jobHandle = taskOneLessonTwo.Schedule();
+            _jobHandle.Complete();
+
+            PrintResult(taskOneLessonTwo.Array);
+            _array.Dispose();
+        }
+
+        private void PrintResult(NativeArray<int> array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Debug.Log($"After [{i}] = {array[i]}");
+            }
+        }
+    }
+}
---
+++ System-programming-in-CSharp-in-Unity/Assets/Lesson2/Task1/Scripts/TaskOneLessonTwo.cs
@@ -0,0 +1,21 @@
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace LessonTwo.TaskOne
+{
+    public struct TaskOneLessonTwo : IJob
+    {
+        public NativeArray<int> Array;
+
+        public void Execute()
+        {
+            for(int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] > 10)
+                {
+                    Array[i] = 0;
+                }
+            }
+        }
+    }
+}
