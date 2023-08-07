e55ce919d23b07ac36df4e34ef17b68528669fac
Artyom Dyomin <kreckop1@gmail.com>
2023-08-07 20:31:37 +1000

    Lesson8StartHW

---
+++ System-programming-in-CSharp-in-Unity/Assets/Lesson8/Scripts/CameraRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Lesson8
+{
+    public class CameraRenderer
+    {
+        private ScriptableRenderContext _context;
+        private Camera _camera;
+
+        private readonly CommandBuffer _commandBuffer = new CommandBuffer{ name = bufferName };
+        private const string bufferName = "Camera Render";
+
+
+        public void Render(ScriptableRenderContext context, Camera camera)
+        {
+            _camera = camera;
+            _context = context;
+
+            DrawVisible();
+            Settings();
+            Submit();
+        }
+
+        private void DrawVisible()
+        {
+            _context.SetupCameraProperties(_camera);
+            _commandBuffer.ClearRenderTarget(true, true, Color.clear);
+            _commandBuffer.BeginSample(bufferName);
+            ExecuteCommandBuffer();
+            _context.DrawSkybox(_camera);
+        }
+
+        private void Settings()
+        {
+            _context.SetupCameraProperties(_camera);
+        }
+
+        private void Submit()
+        {
+            _commandBuffer.EndSample(bufferName);
+            ExecuteCommandBuffer();
+            _context.Submit();
+        }
+
+        private void ExecuteCommandBuffer()
+        {
+            _context.ExecuteCommandBuffer(_commandBuffer);
+            _commandBuffer.Clear();
+        }
+    }
+}
---
+++ System-programming-in-CSharp-in-Unity/Assets/Lesson8/Scripts/CustomRenderPipeline/SpaceRunPipelineRender.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Lesson8.CustomRenderPipeline
+{
+    public class SpaceRunPipelineRender : RenderPipeline
+    {
+        private CameraRenderer _cameraRenderer;
+
+
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            CamerasRender(context, cameras);
+        }
+
+        private void CamerasRender(ScriptableRenderContext context, Camera[] cameras)
+        {
+            foreach (Camera camera in cameras)
+            {
+                _cameraRenderer.Render(context, camera);
+            }
+        }
+    }
+}
